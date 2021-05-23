cdf2b6523bf18d89cb15114d76f446b3b6062efc
Maxb <bittmanmax@gmail.com>
2021-05-22 18:05:31 -0700

    Configure snowflake-proxy behind nat-router
    This is a WIP-ish change which for the moment comments out the snowbox
    element and configures just the snowflake-proxy to run behind a
    nat-router.

--- start-scripts/nat-router-start.sh
+++ start-scripts/nat-router-start.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Identify which interfaces are "lan" vs "wan"
-LAN_ADDR=$NAT_ROUTER_LAN_ADDR
-WAN_ADDR=$NAT_ROUTER_WAN_ADDR
-
 LAN_INTERFACE=$(ip addr | grep "$LAN_ADDR" | awk '{ print $NF }')
 WAN_INTERFACE=$(ip addr | grep "$WAN_ADDR" | awk '{ print $NF }')
 
