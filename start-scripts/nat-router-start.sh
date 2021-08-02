a17f0dcc9347a7b04a23f32f62ba2addd9df2a7d
Maxb <bittmanmax@gmail.com>
2021-08-02 07:59:12 -0400

    Add environment variable for RANDOMIZE_NAT

--- start-scripts/nat-router-start.sh
+++ start-scripts/nat-router-start.sh
@@ -8,9 +8,22 @@ WAN_INTERFACE=$(ip addr | grep "$WAN_ADDR" | awk '{ print $NF }')
 ip route del default
 ip route add default via "$WAN_HOST_GATEWAY"
 
-iptables -t nat -A POSTROUTING -o "$WAN_INTERFACE" -j MASQUERADE
 
-# An attempt at different NAT type here:
-# iptables -t nat -A POSTROUTING -s "$LAN_SUBNET/$LAN_SUFFIX" -j MASQUERADE --random
+if [ "$RANDOMIZE_NAT" = "false" ]; then
+  # Port Restricted Cone NAT;
+  # NAT mapping behavior: endpoint-independent
+  # NAT filtering behavior: address and port-dependent
+  # (AI, AP)
+  iptables -t nat -A POSTROUTING -o "$WAN_INTERFACE" -j SNAT --to-source "$WAN_ADDR"
+else
+# Symmetric NAT;
+# NAT mapping behavior: address-dependent
+# NAT filtering behavior: address and port-dependent
+# (AO, AP)
+  iptables -t nat -A POSTROUTING -o "$WAN_INTERFACE" -j MASQUERADE --random
+  iptables -P FORWARD DROP
+  iptables -A FORWARD -i "$WAN_INTERFACE" -o "$LAN_INTERFACE" -m state --state RELATED,ESTABLISHED -j ACCEPT
+  iptables -A FORWARD -o "$WAN_INTERFACE" -i "$LAN_INTERFACE" -j ACCEPT
+fi
 
 sleep infinity
